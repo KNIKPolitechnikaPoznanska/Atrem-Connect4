1a7331c616d5e23dcde65ff2e5b2284198aa5993
Patryk <Patryk@10.7.7.152>
2014-08-19 13:15:43 +0200

    gui

--- atrem/src/atrem/GUI.java
+++ atrem/src/atrem/GUI.java
@@ -1,8 +1,13 @@
 package atrem;
 
 public class GUI {
-	public void drawBoard(Board PlanszaGry) {
-
+	public void drawBoard(Board BoardTmp) {
+		HoleState HoleTmp;
+		for (int i = 0; i < BoardTmp.Slots; i++) {
+			for (int j = 0; j < BoardTmp.Rows; j++) {
+				HoleTmp = BoardTmp.getHoleState(i, j);
+				System.out.print("|" + HoleTmp + "|");
+			}
+		}
 	}
-
 }
