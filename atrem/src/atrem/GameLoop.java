38de1e8a8cae3380bf06a09f3d94a39e062372cc
Lucas <llukas1994@wp.pl>
2014-08-19 15:40:03 +0200

    GameLoop update

--- atrem/src/atrem/GameLoop.java
+++ atrem/src/atrem/GameLoop.java
@@ -1,6 +1,21 @@
 package atrem;
 
+import java.util.Scanner;
+
 public class GameLoop {
+	private int kolejGracza = 1;
+	private Logic logic;
+	private int slot;
+	Scanner in = new Scanner(System.in);
+
+	public int getKolejGracza() {
+		return kolejGracza;
+	}
+
+	public void setKolejGracza(int kolejGracza) {
+		this.kolejGracza = kolejGracza;
+	}
+
 	Board board;
 	Player player1, player2;
 	private GUI gui = new GUI();
@@ -12,6 +27,17 @@ public class GameLoop {
 		while (true) { // g³ówna pêtla kolejek graczy
 			gui.drawBoard(board);
 
+			if (getKolejGracza() == 1) {
+				slot = in.nextInt();
+				player1.go(slot, board);
+				logic = new Logic(board);
+				setKolejGracza(2);
+			} else {
+				slot = in.nextInt();
+				player1.go(slot, board);
+				logic.checkIfWin();
+				setKolejGracza(1);
+			}
 		}
 	}
 }
