2e46ce58c8222e3d9e721dabc9c38e7b41c4ce4f
Wojtek <Wojtek@10.7.7.154>
2014-08-19 13:33:05 +0200

    bla

--- atrem/src/atrem/Board.java
+++ atrem/src/atrem/Board.java
@@ -1,12 +1,21 @@
 package atrem;
 
 public class Board {
-	public static int Slots = 4;
-	public static int Rows = 4;
-	public static Hole[][] gameBoard = new Hole[Slots][Rows];
+	private int Slots = 7;
+	private int Rows = 6;
+	private Hole[][] gameBoard = new Hole[Slots][Rows];
 
 	public Board() {
 
+		for (int i = 0; i < Slots; i++)
+			for (int j = 0; j < Rows; j++)
+				gameBoard[i][j] = new Hole();
+
+	}
+
+	public Board(int Slots, int Rows) {
+		this.Slots = Slots;
+		this.Rows = Rows;
 		for (int i = 0; i < Slots; i++)
 			for (int j = 0; j < Rows; j++)
 				gameBoard[i][j] = new Hole();
