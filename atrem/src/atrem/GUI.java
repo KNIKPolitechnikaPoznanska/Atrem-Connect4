0117e322760a9bad7629ba4b0228fdff85dc6af8
Patryk <Patryk@10.7.7.152>
2014-08-19 13:00:26 +0200

    gui

--- atrem/src/atrem/GUI.java
+++ atrem/src/atrem/GUI.java
@@ -1,20 +1,8 @@
 package atrem;
 
 public class GUI {
+	public void drawBoard(Board PlanszaGry) {
 
-	public static void rysuj(Hole[][] planszaGry) {
-		for (int i = 0; i < planszaGry.length; i++) {
-			for (int j = 0; j < planszaGry[i].length; j++) {
-				if (planszaGry[i][j].getStanDziury() == HoleState.EMPTY)
-					System.out.print("x");
-				else if (planszaGry[i][j].getStanDziury() == HoleState.PLAYER1)
-					System.out.print("1");
-				else if (planszaGry[i][j].getStanDziury() == HoleState.PLAYER2)
-					System.out.print("2");
-			}
-			System.out.println("");
-
-		}
 	}
 
 }
