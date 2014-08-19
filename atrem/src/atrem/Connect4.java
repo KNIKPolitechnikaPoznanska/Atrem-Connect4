5e13ff4b91c83f81f9e9809c9d7fafd698570986
Mieloch <Mieloch@10.7.7.151>
2014-08-19 12:49:26 +0200

    hthrt

--- atrem/src/atrem/Connect4.java
+++ atrem/src/atrem/Connect4.java
@@ -3,7 +3,9 @@ package atrem;
 public class Connect4 {
 
 	public static void main(String[] args) {
-		PlanszaAlt plansza
-
+		PlanszaAlt planszaGry = new PlanszaAlt();
+		GUI.rysuj(planszaGry.getPlanszaGry());
+		planszaGry.getPlanszaGry()[2][2].setStanDziury(1);
+		GUI.rysuj(planszaGry.getPlanszaGry());
 	}
 }
