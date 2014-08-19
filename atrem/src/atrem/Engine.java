6505c629775cf33bdc049f4efe9857d0991243b8
Mieloch <Mieloch@10.7.7.151>
2014-08-19 15:08:25 +0200

    poprawka engine

--- atrem/src/atrem/Engine.java
+++ atrem/src/atrem/Engine.java
@@ -3,13 +3,16 @@ package atrem;
 public class Engine {
 
 	public int findFreeSpot(int slot, Board board) {
-		for (int i = 0; i < board.getRows(); i++)// rows zmienna
+		int i = 0;
+		for (i = 0; i < board.getRows(); i++)// rows zmienna
 		{
 			HoleState holeState = board.getHoleState(i, slot);
+			if (i == 0 && holeState != HoleState.EMPTY)
+				return -1;
 			if (holeState != HoleState.EMPTY) {
 				return i - 1; // dla pelnego rowka zwraca -1
 			}
 		}
-		return -1;
+		return i;
 	}
 }
