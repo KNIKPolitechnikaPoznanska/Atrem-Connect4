b423ccb2f1830ca277180fc221f10b2488caa99d
Lucas <llukas1994@wp.pl>
2014-08-19 14:29:58 +0200

    Logic - Win

--- atrem/src/atrem/Logic.java
+++ atrem/src/atrem/Logic.java
@@ -1,10 +1,65 @@
 package atrem;
 
 public class Logic {
-
-	public Logic() {
-		// TODO Auto-generated method stub
+	private Board board;
+	private boolean is4 = false;
+	private int rows;
+	private int slots;
 
 	}
+
+	public Logic() {
+		slots = board.getSlots();
+		rows = board.getRows();
+		this.checkIfWin();
+	}
+			public void checkIfWin() {
+
+				for (int row = 0; row < rows; row++) {
+					for (int slot = 0; slot < slots - 3; slot++) {
+						HoleState token = board.getHoleState(row, slot);
+						if (token.getNumber() > 0 && token == board.getHoleState(row, slot + 1)
+								&& token == board.getHoleState(row, slot + 2)
+								&& token == board.getHoleState(row, slot + 3)) {
+							showWinner(board.getHoleState(row, slot));
+						}
+					}
+				}
+
+				for (int slot = 0; slot < slots; slot++) {
+					for (int row = 0; row < rows - 3; row++) {
+						HoleState token = board.getHoleState(row, slot);
+						if (token.getNumber() > 0 && token == board.getHoleState(row+1, slot)
+								&& token == board.getHoleState(row+2, slot)
+								&& token == board.getHoleState(row+3, slot))
+							showWinner(board.getHoleState(row, slot));
+					}
+				}
+
+				for (int row = 0; row < rows - 3; row++) {
+					for (int slot = 0; slot < slots - 3; slot++) {
+						HoleState token = board.getHoleState(row, slot);
+						if (token.getNumber() > 0 && token == board.getHoleState(row+1, slot+1)
+								&& token == board.getHoleState(row+2, slot+2)
+								&& token == board.getHoleState(row+3, slot+3))
+							showWinner(board.getHoleState(row, slot));
+					}
+				}
+
+				for (int row = rows - 1; row >= 3; row--) {
+					for (int slot = 0; slot < slots - 3; slot++) {
+						HoleState token = board.getHoleState(row, slot);
+						if (token.getNumber() > 0 && token == board.getHoleState(row-1, slot+1)
+								&& token == board.getHoleState(row-2, slot+2)
+								&& token == board.getHoleState(row-3, slot+3))
+							showWinner(board.getHoleState(row, slot));
+				}
+		}
+	}
+
+	private void showWinner(HoleState holeState) {
+
+
+}
 
 }
