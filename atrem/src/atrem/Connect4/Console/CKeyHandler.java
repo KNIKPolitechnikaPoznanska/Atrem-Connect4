7460fc61ed0790e620fcce3ab4f90375c4069211
Lucas <llukas1994@wp.pl>
2014-08-20 15:08:58 +0200

    KeyHandler repair

--- atrem/src/atrem/Connect4/Console/CKeyHandler.java
+++ atrem/src/atrem/Connect4/Console/CKeyHandler.java
@@ -12,18 +12,31 @@ public class CKeyHandler extends KeyAdapter {
 	}
 
 	public int getSlot(Board board) {
+		slot = 0;
+
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
-		while (true) {
-			slot = input.nextInt();
-			if (slot > 0 && slot < board.getSlots()) {
-				return slot;
-			} else {
-				System.out.println("Wprowadzi³eœ z³y slot!");
-				System.out.println("WprowadŸ slot z zakresu [0-"
-						+ board.getSlots() + "]");
-			}
 
+		while (true) {
+			boolean bError = true;
+			while (bError) {
+				if (input.hasNextInt())
+					slot = input.nextInt();
+				else {
+					System.out.println("WprowadŸ liczbê!!!");
+					input.next();
+					continue;
+				}
+				bError = false;
+
+				if (slot > 0 && slot < board.getSlots()) {
+					return slot;
+				} else {
+					System.out.println("Wprowadzi³eœ z³y slot!");
+					System.out.println("WprowadŸ slot z zakresu [0-"
+							+ board.getSlots() + "]");
+				}
+			}
 		}
 	}
 
