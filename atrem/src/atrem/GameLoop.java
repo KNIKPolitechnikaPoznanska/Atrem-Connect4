f19f20b0e9e69df9cd9b9835ced2a8f779ad52b3
Lucas <llukas1994@wp.pl>
2014-08-20 09:34:37 +0200

    Repair GameLoop and Logic

--- atrem/src/atrem/GameLoop.java
+++ atrem/src/atrem/GameLoop.java
@@ -3,27 +3,26 @@ package atrem;
 import java.util.Scanner;
 
 public class GameLoop {
-	private int playerTurn = 1;
-	private Logic logic;
-	private int slot;
-	Scanner in = new Scanner(System.in);
-
-	public int getPlayerTurn() {
-		return playerTurn;
-	}
-
-	public void setPlayerTurn(int kolejGracza) {
-		this.playerTurn = kolejGracza;
-	}
-
 	Board board;
-	Player player1, player2;
 	private GUI gui = new GUI();
+	Scanner in = new Scanner(System.in);
+	private Logic logic;
+	private Player player1, player2;
+	private int playerTurn = 1;
+	private int slot;
 
 	GameLoop(Board board, Player player1, Player player2) {
 		this.player1 = player1;
 		this.player2 = player2;
 		this.board = board;
+		Loop();
+	}
+
+	public int getPlayerTurn() {
+		return playerTurn;
+	}
+
+	private void Loop() {
 		while (true) { // g³ówna pêtla kolejek graczy
 			gui.drawBoard(board);
 
@@ -34,10 +33,16 @@ public class GameLoop {
 				setPlayerTurn(2);
 			} else {
 				slot = in.nextInt();
-				player1.go(slot, board);
-				logic.checkIfWin();
+				player2.go(slot, board);
 				setPlayerTurn(1);
 			}
+			if (logic.checkIfWin())
+				break;
 		}
+
+	}
+
+	public void setPlayerTurn(int playerTurn) {
+		this.playerTurn = playerTurn;
 	}
 }
