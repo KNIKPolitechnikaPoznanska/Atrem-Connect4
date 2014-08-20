2557359e9f7aacc2ab7c3bf242f69d5d5701b308
Lucas <llukas1994@wp.pl>
2014-08-20 13:59:21 +0200

    New Connect4 starting method

--- atrem/src/atrem/Connect4/Connect4.java
+++ atrem/src/atrem/Connect4/Connect4.java
@@ -2,23 +2,22 @@ package atrem.Connect4;
 
 import atrem.Connect4.Console.GameLoop;
 import atrem.Connect4.Console.Menu;
-import atrem.Connect4.Game.Board;
-import atrem.Connect4.Game.Player;
+import atrem.Connect4.Game.Game;
+import atrem.Connect4.Game.GameFactory;
 
 public class Connect4 {
 
-	private static Player player1, player2;
-	private static GameLoop gameloop;
-	private static Board board;
+	private static GameFactory gamefactory;
 	private static Menu menu;
+	private static Game game;
+	private static GameLoop gameloop;
 
 	public static void main(String[] args) {
-		menu = new Menu(board, player1, player2);
-		board = menu.getBoard();
-		player1 = menu.getPlayer1();
-		player2 = menu.getPlayer2();
-		gameloop = new GameLoop(board, player1, player2);// zamienic na met
-															// start
+		menu = new Menu();
+		menu.loadSettings();
 
+		gamefactory.setGameFactory(game, menu.getRows(), menu.getSlots(),
+				menu.getPlayer1name(), menu.getPlayer2name());
+		gameloop.
 	}
 }
