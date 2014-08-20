d9609f5b5cb1c27b6e7331fdfb632b0aa28dfa5a
Wojtek <Wojtek@10.7.7.154>
2014-08-20 13:32:18 +0200

    k

--- atrem/src/atrem/Connect4/Game/GameFactory.java
+++ atrem/src/atrem/Connect4/Game/GameFactory.java
@@ -1,17 +1,15 @@
 package atrem.Connect4.Game;
 
 public class GameFactory {
-	private int rows;
-	private int slots;
+
 	private Board board;
 	private Game game;
 	private Player player1, player2;
 
-	public GameFactory(int rows, int slots) {
-		this.rows = rows;
-		this.slots = slots;
+	public GameFactory(int rows, int slots, String im1) {
+
 		board = new Board(rows, slots);
-		player1 game = new Game();
+		player1 = new Player(im1, HoleState.PLAYER1);
 
 	}
 
