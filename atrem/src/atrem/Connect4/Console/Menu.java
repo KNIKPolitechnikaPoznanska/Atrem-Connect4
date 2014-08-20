11c5c75c28b455eea9bcae6f9b45edf846d60560
dektrab <bartekd11@gmail.com>
2014-08-20 13:37:25 +0200

    menu

--- atrem/src/atrem/Connect4/Console/Menu.java
+++ atrem/src/atrem/Connect4/Console/Menu.java
@@ -1,39 +1,43 @@
 package atrem.Connect4.Console;
 
-import java.util.Scanner;
-
 import atrem.Connect4.Game.Board;
-import atrem.Connect4.Game.HoleState;
 import atrem.Connect4.Game.Player;
 
 public class Menu { // GameFactory
 	private Board board;
-	private Player player1, player2;
+	private String player1name, player2name;
+	private CKeyHandler menu;
+	private int slots, rows;
 
-	public Menu(Board board, Player player1, Player player2) {// rozbicie na
-																// metody
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);
-		System.out.println("Witamy w grze connect 4");
-		System.out.println("Podaj liczbe wierszy");
-		int Rows = in.nextInt();
-		System.out.println("Podaj liczbe slotow");
-		int Slots = in.nextInt();
-		board = new Board(Rows, Slots);
+	public Menu() {
 
-		System.out.println("Podaj imie pierwszego zawodnika");
-		String im = in.next();
-		player1 = new Player(im, HoleState.PLAYER1);
-		System.out.println("Podaj imie drugiego zawodnika");
-		im = in.next();
-		player2 = new Player(im, HoleState.PLAYER2);
-		sendBoard(board, player1, player2);
+		menu = new CKeyHandler();
+		settings();
+
+		sendSettings();
 	}
 
-	public void sendBoard(Board board, Player player1, Player player2) {
-		this.board = board;
-		this.player1 = player1;
-		this.player2 = player2;
+	public void settings() {
+		menu = new CKeyHandler();
+		System.out.println("Witamy w grze connect 4");
+		System.out.println("Podaj liczbe wierszy");
+		rows = menu.getInt();
+		System.out.println("Podaj liczbe slotow");
+		slots = menu.getInt();
+		System.out.println("Podaj imie pierwszego zawodnika");
+		player1name = menu.getString();
+
+		// System.out.println("Podaj imie zawodnika " + player.getName() +
+		// ": "); na przyszlosc dla wielu graczy
+		// player1 = new Player(im, HoleState.PLAYER1);
+
+		System.out.println("Podaj imie drugiego zawodnika");
+		player2name = menu.getString();
+
+	}
+
+	public void sendSettings() {
+
 	}
 
 	public Board getBoard() {
