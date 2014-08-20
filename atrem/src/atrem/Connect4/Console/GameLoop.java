e7cc89e13964bbb0189ccf6a09a8630ae3bffc02
Mieloch <Mieloch@10.7.7.151>
2014-08-20 13:50:20 +0200

    poprawka gameloop

--- atrem/src/atrem/Connect4/Console/GameLoop.java
+++ atrem/src/atrem/Connect4/Console/GameLoop.java
@@ -1,30 +1,28 @@
 package atrem.Connect4.Console;
 
-import java.util.Scanner;
-
 import atrem.Connect4.Game.Board;
-import atrem.Connect4.Game.Logic;
+import atrem.Connect4.Game.Game;
 import atrem.Connect4.Game.Player;
 
 public class GameLoop {
-	Board board;
 	private GUI gui = new GUI();
-	Scanner in = new Scanner(System.in);
-	private Logic logic;
-	private Player player1, player2;
+	private Game game;
 
-	GameLoop(Board board, Player player1, Player player2) {
-		this.player1 = player1;
-		this.player2 = player2;
-		this.board = board;
+	GameLoop(Game game) {
+		this.game = game;
+	}
 
+	public void startLoop() {
+		while (true) {
+
+			return;
+		}
 	}
 
 	public void go(Board board, Player player) {// do gui lub do gameloop
 		int slot = 2; // = metoda CKeyHandler.getSlot
 		int emptySlot = board.findFreeSpot(slot);
 		board.setHoleState(emptySlot, slot, player.getPlayerId()); // gracz
-
 	}
 
 }
