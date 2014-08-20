1d2b1d96cf1bdc0913f89ebb0cf26bf09bfaa07d
Mieloch <Mieloch@10.7.7.151>
2014-08-20 10:01:51 +0200

    poprawka go

--- atrem/src/atrem/Engine.java
+++ atrem/src/atrem/Engine.java
@@ -7,11 +7,13 @@ public class Engine {
 
 		for (i = 0; i < board.getRows(); i++)// rows zmienna
 		{
-			HoleState holeState = board.getHoleState(slot, i);
+			HoleState holeState = board.getHoleState(i, slot);
 			if (i == 0 && holeState != HoleState.EMPTY)
 				return -1;
 			if (holeState != HoleState.EMPTY) {
-				return i - 1; // dla pelnego rowka zwraca -1
+				{
+					return i - 1;
+				}// dla pelnego rowka zwraca -1}
 			}
 		}
 		return i - 1;
--- atrem/src/atrem/Player.java
+++ atrem/src/atrem/Player.java
@@ -33,8 +33,8 @@ public class Player {
 	public void go(int slot, Board board) {
 		Engine engine = new Engine();
 		int emptySlot = engine.findFreeSpot(slot, board);
-		if (emptySlot != -1)
-			board.setGameBoard(emptySlot, slot, playerId); // gracz
+
+		board.setGameBoard(emptySlot, slot, playerId); // gracz
 
 	}
 }
