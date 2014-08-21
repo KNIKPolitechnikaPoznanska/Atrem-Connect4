29c23a39f512b95f5095109567bad336e25b1871
Wojtek <Wojtek@10.7.7.154>
2014-08-21 14:57:42 +0200

    bvcb

---
+++ atrem/src/atrem/Connect4/Game/EasyPC.java
@@ -0,0 +1,47 @@
+package atrem.Connect4.Game;
+
+import java.util.Random;
+
+public class EasyPC extends AI implements PlayerController {
+
+	public EasyPC(String name, HoleState playerId, Board board) {
+		this.name = name;
+		this.playerId = playerId;
+		this.board = board;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public HoleState getPlayerId() {
+		return playerId;
+
+	}
+
+	@Override
+	public int getSlots() {
+		int randomSlot;
+		int choosenSlot;
+		Random rand = new Random();
+		do {
+			randomSlot = rand.nextInt(board.getSlots());
+			choosenSlot = board.findFreeSpot(randomSlot);
+		} while (choosenSlot == -1);
+		return choosenSlot;
+	}
+
+	@Override
+	public int getSlotNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
