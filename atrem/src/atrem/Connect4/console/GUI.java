02c1262aa75ff662448a465cf03d098769af7e2c
Lucas <llukas1994@wp.pl>
2014-08-22 14:29:54 +0200

    revert

---
+++ atrem/src/atrem/Connect4/console/GUI.java
@@ -0,0 +1,80 @@
+package atrem.Connect4.console;
+
+import atrem.Connect4.Game.Board;
+import atrem.Connect4.Game.Game;
+import atrem.Connect4.Game.HoleState;
+import atrem.Connect4.Game.player.PlayerController;
+
+/*
+ * Rysowanie GUI konsolowego
+ * w tym: planszy i jej objaœnieñ
+ */
+public class GUI {
+
+	public void displayGame(Game game) {
+		this.drawBoard(game.getBoard());
+		this.drawSlots(game.getBoard().getSlots());
+		if (game.getPlayerTurn() == 1) {
+			this.writeCurrentPlayer(game.getPlayer1());
+		}
+		if (game.getPlayerTurn() == 2) {
+			this.writeCurrentPlayer(game.getPlayer2());
+		}
+		this.displayParagraph();
+	}
+
+	public void displayResults(Game game) {
+		this.drawBoard(game.getBoard());
+		this.displayParagraph();
+	}
+
+	private void drawBoard(Board BoardTmp) {
+		HoleState HoleTmp;
+		for (int j = 0; j < BoardTmp.getRows(); j++) {
+			for (int i = 0; i < BoardTmp.getSlots(); i++) {
+
+				HoleTmp = BoardTmp.getHoleState(j, i);
+				System.out.print("| " + HoleTmp.getNumber() + " |");
+			}
+			System.out.println("");
+		}
+
+	}
+
+	private void drawSlots(int slots) {
+		System.out.println("Numery slotów:");
+		for (int i = 1; i <= slots; i++) {
+			System.out.print("| " + i + " |");
+		}
+		System.out.println();
+
+	}
+
+	private void writeCurrentPlayer(PlayerController player) {
+		String name = player.getName();
+		System.out.println("Obecny ruch wykonuje:  " + name + " ");
+	}
+
+	private void displayParagraph() {
+		System.out.println("\n");
+		System.out.println("************************************");
+	}
+
+	public void writeFullSlots() {
+		System.out.println("Slot jest pelen, podaj inny: ");
+	}
+
+	public boolean showWinner(HoleState holeState, Game game) {
+		if (holeState.name() == "PLAYER1") {
+			System.out.println("	Wygra³: " + game.getNamePlayer1());
+		} else if (holeState.name() == "PLAYER2") {
+			System.out.println("Wygra³ " + game.getNamePlayer2());
+		}
+		return true;
+	}
+
+	public void showRemis() {
+		System.out.println("Gra bez rostrzygniecia \n Remis.");
+	}
+
+}
---
+++ atrem/src/atrem/Connect4/console/GameLoop.java
@@ -0,0 +1,67 @@
+package atrem.Connect4.console;
+
+import java.util.Random;
+
+import atrem.Connect4.Game.Board;
+import atrem.Connect4.Game.Game;
+import atrem.Connect4.Game.Logic;
+import atrem.Connect4.Game.player.PlayerController;
+
+/*
+ * G³ówna pêtla gry
+ * wykorzystywana tylko w konsoli
+ * obs³uguj¹ca kolejki graczy
+ */
+public class GameLoop {
+	private int doneMoves;
+	private Game game;
+	private GUI gui = new GUI();
+	private Logic logic;
+	private Random rnd = new Random();
+
+	public GameLoop(Game game) {
+		this.game = game;
+		logic = new Logic(game);
+		game.setPlayerTurn(genFirstTurn());
+	}
+
+	private int genFirstTurn() {
+		if (rnd.nextGaussian() < 0.0D) {
+			return 1;
+		} else {
+			return 2;
+		}
+	}
+
+	public void go(Board board, PlayerController player) {
+		int emptySlot;
+		int slot;
+		do {
+			slot = player.getSlotNumber(); // = metoda
+											// CKeyHandler.getSlot
+			emptySlot = board.findFreeSpot(slot);
+			if (emptySlot == -1) {
+				gui.writeFullSlots();
+			}
+		} while (emptySlot == -1);
+		board.setHoleState(emptySlot, slot, player.getPlayerId()); // gracz
+	}
+
+	public void startLoop() {
+		doneMoves = 0;
+		while (!logic.checkIfWin() && !logic.checkIfDraw(doneMoves)) {
+			gui.displayGame(game);
+			if (game.getPlayerTurn() == 1) {
+				this.go(game.getBoard(), game.getPlayer1());
+				game.setPlayerTurn(2);
+			} else if (game.getPlayerTurn() == 2) {
+				this.go(game.getBoard(), game.getPlayer2());
+				game.setPlayerTurn(1);
+			}
+			doneMoves++;
+
+		}
+		gui.displayResults(game);
+	}
+
+}
