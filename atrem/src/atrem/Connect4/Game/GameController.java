97a79bfc529296a29fbe5bdf0afe209a73d3d1ae
Wojtek <Wojtek@10.7.7.154>
2014-08-25 15:12:28 +0200

    zmiany

--- atrem/src/atrem/Connect4/Game/GameController.java
+++ atrem/src/atrem/Connect4/Game/GameController.java
@@ -1,7 +1,35 @@
 package atrem.Connect4.Game;
 
-import atrem.Connect4.Game.board.HoleState;
+import atrem.Connect4.Game.board.Board;
+import atrem.Connect4.console.GUIConsole;
 
-public interface GameController {
-	public void wykonalemRuch(HoleState playerId, int slot);
+public class GameController {
+	// public void wykonalemRuch(HoleState playerId, int slot);\
+	private Game game;
+	private Logic logic;
+	private Board board;
+	private int doneMoves;
+	private GUIConsole gui;
+
+	public void setGameController(Game game, Logic logic, Board board) {
+		this.game = game;
+		this.logic = logic;
+		this.board = board;
+	}
+
+	public void Loop() { // wywalic z konsoli
+		doneMoves = 0;
+		while (!logic.checkResult(doneMoves)) {
+			// gui.displayGame(game);
+			if (game.getPlayerTurn() == 1) {
+				board.go(player1);
+				game.setPlayerTurn(2);
+			} else if (game.getPlayerTurn() == 2) {
+				board.go(player2);
+				game.setPlayerTurn(1);
+			}
+			doneMoves++;
+		}
+		// gui.displayResults(game);
+	}
 }
