d4bc4406ed92cab319f004ef60237ef8c34cb4bd
dektrab <bartekd11@gmail.com>
2014-08-27 10:20:21 +0200

    wygladajace gui, guziki chodzą, ikony nadal w toku

---
+++ atrem/src/Test/SlotPanel.java
@@ -0,0 +1,26 @@
+package Test;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SlotPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1429466827813271904L;
+	ResourceLoader iconResource = new ResourceLoader();
+
+	/**
+	 * Create the panel.
+	 */
+	public SlotPanel(int rows) {
+
+		JLabel[] HoleLabel = new JLabel[rows];
+		for (int rowNum = 0; rowNum < rows; rowNum++) {
+			HoleLabel[rowNum] = new JLabel();
+			add(HoleLabel[rowNum]);
+			HoleLabel[rowNum].setIcon(iconResource.get(0));
+
+		}
+	}
+}
