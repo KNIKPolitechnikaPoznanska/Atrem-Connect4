bf931feb5df31339d4de677e21603e4f4a7fe15f
Wojtek <Wojtek@10.7.7.154>
2014-08-28 09:11:43 +0200

    SwingPresenter gotowy

---
+++ atrem/src/atrem/Connect4/swing/SwingPresenter.java
@@ -0,0 +1,46 @@
+package atrem.Connect4.swing;
+
+import atrem.Connect4.Game.Game;
+import atrem.Connect4.Game.GameController;
+import atrem.Connect4.Game.ResultState;
+import atrem.Connect4.Game.board.Board;
+
+public class SwingPresenter {
+	private Game game;
+	private int Slots;
+	private int Rows;
+	private int PlayerTurn;
+	private int LastRow;
+	private int LastSlot;
+	private ResultState result;
+	private GameController gameController;
+
+	public void setPresenter(Game game, GameController gameController) {
+		this.game = game;
+		this.gameController = gameController;
+	}
+
+	public void runSettingsView() { // wywowlujemy okno dialogowe z ustawieniamy
+									// row slot
+
+	}
+
+	public void setSettings(int Rows, int Slots) {
+		this.Rows = Rows;
+		this.Slots = Slots;
+	}
+
+	public void runGameView(int Rows, int Slots, Board board) { // wyowlujemy
+																// okno gry
+
+	}
+
+	public void goView(int LastRow, int LastSlot, ResultState result) {
+
+	}
+
+	public void getTurnFromView(int LastSlot) {
+		gameController.setChoosedSlot(LastSlot);
+	}
+
+}
