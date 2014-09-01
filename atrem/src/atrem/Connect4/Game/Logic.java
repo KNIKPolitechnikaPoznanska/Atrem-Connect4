f91d07726ace7bdc7b5f35c51e3f333bd828f423
Lucas <llukas1994@wp.pl>
2014-09-01 13:12:38 +0200

    Rozbicie Logica checkWin

--- atrem/src/atrem/Connect4/Game/Logic.java
+++ atrem/src/atrem/Connect4/Game/Logic.java
@@ -11,87 +11,14 @@ public class Logic {
 	private GameController gameController;
 	HoleState token;
 	private boolean hasWinner = false;
+	private boolean cpuWin = false;
+	private String human = "human", cpu = "cpu";
 
 	public Logic(GameController gameController) {
 		this.gameController = gameController;
 		maxSlots = gameController.getBoard().getSlots();
 		maxRows = gameController.getBoard().getLastRow();
-		// this.game = game;
 		// this.checkIfWin();
-	}
-
-	public boolean checkResult(int doneMoves) {
-		checkIfWin();
-		checkIfDraw(doneMoves);
-		if (gameController.getResult() == ResultState.NoWin)
-			return false;
-		return true;
-	}
-
-	/*
-	 * Sprawdzenie u³o¿enia 4 w 4 wariantach
-	 */
-	public boolean checkIfWin() {
-		for (int row = 0; row < maxRows; row++) { // slot\kolumna
-			for (int slot = 0; slot < maxSlots - 3; slot++) {
-				token = gameController.getHoleState(row, slot);
-				if (token.getNumber() > 0
-						&& token == gameController.getHoleState(row, slot + 1)
-						&& token == gameController.getHoleState(row, slot + 2)
-						&& token == gameController.getHoleState(row, slot + 3)) {
-					// hasWinner = gui.showWinner(token, game);
-					setPlayerWon();
-				}
-			}
-		}
-
-		for (int slot = 0; slot < maxSlots; slot++) { // wiersz
-			for (int row = 0; row < maxRows - 3; row++) {
-				token = gameController.getBoard().getHoleState(row, slot);
-				if (token.getNumber() > 0
-						&& token == gameController.getHoleState(row + 1, slot)
-						&& token == gameController.getHoleState(row + 2, slot)
-						&& token == gameController.getHoleState(row + 3, slot))
-					setPlayerWon();
-			}
-		}
-
-		for (int row = 0; row < maxRows - 3; row++) { // skos /
-			for (int slot = 0; slot < maxSlots - 3; slot++) {
-				token = gameController.getBoard().getHoleState(row, slot);
-				if (token.getNumber() > 0
-						&& token == gameController.getHoleState(row + 1,
-								slot + 1)
-						&& token == gameController.getHoleState(row + 2,
-								slot + 2)
-						&& token == gameController.getHoleState(row + 3,
-								slot + 3))
-					setPlayerWon();
-			}
-		}
-
-		for (int row = maxRows - 1; row >= 3; row--) { // skos \
-			for (int slot = 0; slot < maxSlots - 3; slot++) {
-				HoleState token = gameController.getHoleState(row, slot);
-				if (token.getNumber() > 0
-						&& token == gameController.getHoleState(row - 1,
-								slot + 1)
-						&& token == gameController.getHoleState(row - 2,
-								slot + 2)
-						&& token == gameController.getHoleState(row - 3,
-								slot + 3))
-					setPlayerWon();
-			}
-		}
-		return hasWinner;
-	}
-
-	private void setPlayerWon() {
-		if (token.getNumber() == 1) {
-			gameController.setResult(ResultState.Player1Win);
-		} else if (token.getNumber() == 2) {
-			gameController.setResult(ResultState.Player2Win);
-		}
 	}
 
 	public boolean checkIfDraw(int doneMoves) {
@@ -102,7 +29,34 @@ public class Logic {
 		return false;
 	}
 
+	/*
+	 * Sprawdzenie u³o¿enia 4 w 4 wariantach
+	 */
+	public boolean checkIfWin() {
+		columnWin(human);
+		rowWin(human);
+		diagonalWin1(human);
+		diagonalWin2(human);
+		return hasWinner;
+	}
+
 	public boolean checkIfWinPC() {
+		columnWin(cpu);
+		rowWin(cpu);
+		diagonalWin1(cpu);
+		diagonalWin2(cpu);
+		return cpuWin;
+	}
+
+	public boolean checkResult(int doneMoves) {
+		checkIfWin();
+		checkIfDraw(doneMoves);
+		if (gameController.getResult() == ResultState.NoWin)
+			return false;
+		return true;
+	}
+
+	private void columnWin(String playerType) {
 		for (int row = 0; row < maxRows; row++) { // slot\kolumna
 			for (int slot = 0; slot < maxSlots - 3; slot++) {
 				token = gameController.getHoleState(row, slot);
@@ -110,24 +64,13 @@ public class Logic {
 						&& token == gameController.getHoleState(row, slot + 1)
 						&& token == gameController.getHoleState(row, slot + 2)
 						&& token == gameController.getHoleState(row, slot + 3)) {
-					// hasWinner = gui.showWinner(token, game);
-
-					return true;
+					setPlayerWon(playerType);
 				}
 			}
 		}
+	}
 
-		for (int slot = 0; slot < maxSlots; slot++) { // wiersz
-			for (int row = 0; row < maxRows - 3; row++) {
-				token = gameController.getBoard().getHoleState(row, slot);
-				if (token.getNumber() > 0
-						&& token == gameController.getHoleState(row + 1, slot)
-						&& token == gameController.getHoleState(row + 2, slot)
-						&& token == gameController.getHoleState(row + 3, slot))
-					return true;
-			}
-		}
-
+	private void diagonalWin1(String playerType) {
 		for (int row = 0; row < maxRows - 3; row++) { // skos /
 			for (int slot = 0; slot < maxSlots - 3; slot++) {
 				token = gameController.getBoard().getHoleState(row, slot);
@@ -138,10 +81,12 @@ public class Logic {
 								slot + 2)
 						&& token == gameController.getHoleState(row + 3,
 								slot + 3))
-					return true;
+					setPlayerWon(playerType);
 			}
 		}
+	}
 
+	private void diagonalWin2(String playerType) {
 		for (int row = maxRows - 1; row >= 3; row--) { // skos \
 			for (int slot = 0; slot < maxSlots - 3; slot++) {
 				HoleState token = gameController.getHoleState(row, slot);
@@ -152,9 +97,32 @@ public class Logic {
 								slot + 2)
 						&& token == gameController.getHoleState(row - 3,
 								slot + 3))
-					return true;
+					setPlayerWon(playerType);
 			}
 		}
-		return false;
+	}
+
+	private void rowWin(String playerType) {
+		for (int slot = 0; slot < maxSlots; slot++) { // wiersz
+			for (int row = 0; row < maxRows - 3; row++) {
+				token = gameController.getBoard().getHoleState(row, slot);
+				if (token.getNumber() > 0
+						&& token == gameController.getHoleState(row + 1, slot)
+						&& token == gameController.getHoleState(row + 2, slot)
+						&& token == gameController.getHoleState(row + 3, slot))
+					setPlayerWon(playerType);
+			}
+		}
+	}
+
+	private void setPlayerWon(String playerType) {
+		if (playerType == cpu) {
+			cpuWin = true;
+		}
+		if (token.getNumber() == 1) {
+			gameController.setResult(ResultState.Player1Win);
+		} else if (token.getNumber() == 2) {
+			gameController.setResult(ResultState.Player2Win);
+		}
 	}
 }
