8b8c0074bd1eead208df97fbcc1a3e95156c2063
dektrab <bartekd11@gmail.com>
2014-09-01 08:43:21 +0200

    rozbicie panelu gry w celu potencjalnie latwiejszej edycji wyswietlanie kolejki graczy za pomocą ikony (klasa Boczny) okienko do komunikacji z użytkownikiem 'info'

--- atrem/src/Test/C4gui.java
+++ atrem/src/Test/C4gui.java
@@ -1,15 +1,10 @@
 package Test;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class C4gui extends JFrame {
 
@@ -35,86 +30,18 @@ public class C4gui extends JFrame {
 	 */
 	public C4gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 549, 300);
+		setBounds(100, 100, 552, 331);
 
 		Container mainContainer = getContentPane();
-		Test.Panel panel = new Panel();
+		getContentPane().setLayout(null);
+		Panel panel = new Panel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(10, 0, 330, 282);
 		mainContainer.add(panel);
+		Boczny boczny = new Boczny();
+		boczny.setBounds(340, 0, 186, 262);
+
+		getContentPane().add(boczny);
 
 	}
 }
-
-class Panel extends JPanel {
-
-	int rows = 6;
-	int slots = 7;
-	int chosenSlot;
-	JLabel[][] Plansza = new JLabel[rows][slots];
-	JButton[] button = new JButton[slots];
-	// private ResourceLoader iconResource;
-	// ActionListener newMouve = new placeToken();
-	ResourceLoader iconResource = new ResourceLoader();
-
-	public Panel() {
-
-		setLayout(new GridLayout(rows + 1, slots));
-
-		for (int tempSlot = 0; tempSlot < slots; tempSlot++) {
-			button[tempSlot] = new JButton("" + (tempSlot + 1));
-			add(button[tempSlot]);
-
-			button[tempSlot].addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent TokenPlaced) {
-					Object s = TokenPlaced.getSource();
-					for (int tempSlot = 0; tempSlot < slots; tempSlot++) {
-						if (s == button[tempSlot]) {
-							chosenSlot = tempSlot;
-							int PlayerID = 1;
-							int freeRow = 4;
-							Plansza[freeRow][tempSlot].setIcon(iconResource
-									.get(PlayerID));
-							// w miejsce 0 - method nextFreeRow()
-						}
-					}
-				}
-			});
-		}
-		/**
-		 * rowsy z slotsami zamienione miejscami na potrzebe jlabela
-		 */
-		for (int tempRow = 0; tempRow < rows; tempRow++) {
-			for (int tempSlot = 0; tempSlot < slots; tempSlot++) {
-				Plansza[tempRow][tempSlot] = new JLabel();
-				add(Plansza[tempRow][tempSlot]);
-				iconResource.setLabelH((int) Plansza[tempRow][tempSlot]
-						.getSize().getHeight());
-				iconResource.setLabelW((int) Plansza[tempRow][tempSlot]
-						.getSize().getWidth());
-				Plansza[tempRow][tempSlot].setIcon(iconResource.get(0));
-			}
-		}
-
-		// int changeIconInSlot(int slots, int rows, int iconNumber)
-		// {
-		//
-		// }
-	}
-
-	// void placeTokenInSlot(int slot) {
-	// // Plansza[1][1].setIcon(iconResource.get(1));
-	// Plansza[i][j].setText("chuj");
-	// }
-
-	public int getGUISlot() {
-		return chosenSlot;
-	}
-}
-
-/*
- * int PlayerID; switch(PlayerID){ case 0:
- * Plansza[0][tempSlot].setIcon(iconResource.get(0)); return; case 1:
- * Plansza[0][tempSlot].setIcon(iconResource.get(1)); return; case 2:
- * Plansza[0][tempSlot].setIcon(iconResource.get(2)); return; default:
- * System.out.println("Tu nie powinniœmy siê nigdy znaleŸæ..."); return;
- */
