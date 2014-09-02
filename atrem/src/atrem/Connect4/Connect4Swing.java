c1ce25ef6439b2ac41d03ed1fb34ec6af11ff41e
Patryk <Patryk@10.7.7.152>
2014-09-02 08:30:10 +0200

    b

--- atrem/src/atrem/Connect4/Connect4Swing.java
+++ atrem/src/atrem/Connect4/Connect4Swing.java
@@ -2,9 +2,7 @@ package atrem.Connect4;
 
 import atrem.Connect4.Game.GameController;
 import atrem.Connect4.Game.GameFactory;
-import atrem.Connect4.swing.GameFrame;
 import atrem.Connect4.swing.SwingConfig;
-import atrem.Connect4.swing.SwingPresenter;
 
 /*
  * Klasa main uruchamiaj¹ca grê Connect4
@@ -13,25 +11,24 @@ public class Connect4Swing implements Runnable {
 	private static GameFactory gameFactory;
 	private static SwingConfig config;
 	private static GameController gameController;
-	private static SwingPresenter swingPresenter1, swingPresenter2;
 
 	public static void main(String[] args) {
 		gameFactory = new GameFactory();
 		InitializeConfig();
 	}
 
-	private static synchronized void runGame() {
+	private static void runGame() {
 		gameFactory.setSwingconfig(config);
 		gameFactory.createGame("swing");
 		gameFactory.loadGameController();
 		gameController = gameFactory.getGameController();
-		try {
-			GameFrame frame = new GameFrame();
-			frame.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// gameController.gameLoop();
+		// try {
+		// GameFrame frame = new GameFrame();
+		// frame.setVisible(true);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// // gameController.gameLoop();
 	}
 
 	private static void InitializeConfig() {
