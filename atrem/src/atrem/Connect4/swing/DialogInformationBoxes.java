a9af08115187d35feddaee22072e30fdb69e984c
Grzegorz <Admin@10.7.7.204>
2014-09-03 14:30:45 +0200

    dialogi wygrana, remis

--- atrem/src/atrem/Connect4/swing/DialogInformationBoxes.java
+++ atrem/src/atrem/Connect4/swing/DialogInformationBoxes.java
@@ -1,17 +1,22 @@
 package atrem.Connect4.swing;
 
+import javax.swing.JOptionPane;
+
 import atrem.Connect4.Game.PlayerId;
 
 public class DialogInformationBoxes {
 	
 	public void winMessage(PlayerId playerTurn){
 		
+		JOptionPane.showMessageDialog(null,"WYGRANA!");
 	}
 	
 	public void drawMessage(){
-		
+		JOptionPane.showMessageDialog(null,"REMIS!");	
 	}
 	
-	public void fullSlotMessage()
+	public void fullSlotMessage(){
+
+	}
 
 }
