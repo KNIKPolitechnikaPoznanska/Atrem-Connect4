959becabb4ec9ee5fe8cd46d9597e45817c85bdd
Patryk <Patryk@10.7.7.152>
2014-09-03 15:03:07 +0200

    popraw gameCotroller

--- atrem/src/atrem/Connect4/Game/GameController.java
+++ atrem/src/atrem/Connect4/Game/GameController.java
@@ -70,10 +70,10 @@ public class GameController implements Runnable {
 			waitForMove();
 			doneMoves++;
 			resultGame = logic.checkResult(doneMoves);
-			if (resultGame == true) {
-				player1.endOfGame(resultState);
-				player2.endOfGame(resultState);
-			}
+			// if (resultGame == true) {
+			// player1.endOfGame(resultState);
+			// player2.endOfGame(resultState);
+			// }
 			changePlayer();
 		}
 	}
