f5983ffa0a00d38bdef41c946a73c0108d22257d
Mieloch <Mieloch@10.7.7.199>
2014-09-03 13:49:10 +0200

    lepsza logika

---
+++ atrem/src/Test/BoardGenerator.java
@@ -0,0 +1,43 @@
+package Test;
+
+import atrem.Connect4.Game.board.Board;
+import atrem.Connect4.Game.board.HoleState;
+import atrem.Connect4.console.GUIConsole;
+
+public class BoardGenerator {
+	static final int MAX_ROWS = 6, MAX_SLOTS = 7;
+	Board board = new Board(MAX_ROWS, MAX_SLOTS);
+
+	public static void main(String[] args) {
+		BoardGenerator bg = new BoardGenerator();
+		GUIConsole gc = new GUIConsole();
+		bg.setVerticalOrHorizontalWinningFour(2, 2, 1, 5);
+		gc.drawBoard(bg.board);
+
+	}
+
+	public void setDiagonalWinningFour(int startRow, int endRow, int startSlot,
+			int endSlot) {
+		for (int row = startRow, slot = startSlot; row < endRow
+				&& slot < endSlot; row++, slot++) {
+			board.setHoleState(row, slot, HoleState.PLAYER1);
+		}
+	}
+
+	public void setVerticalOrHorizontalWinningFour(int startRow, int endRow,
+			int startSlot, int endSlot) {
+		if (startRow == endRow)
+			for (int slot = startSlot; slot < endSlot; slot++) {
+				board.setHoleState(startRow, slot, HoleState.PLAYER1);
+			}
+		else if (startRow == endRow)
+			for (int row = startRow; row < endRow; row++) {
+				board.setHoleState(row, startSlot, HoleState.PLAYER1);
+			}
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+}
