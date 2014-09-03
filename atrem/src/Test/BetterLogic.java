24f2bd30b9dd90e8b114b389a53253a978f91ea5
Mieloch <Mieloch@10.7.7.199>
2014-09-03 14:35:34 +0200

    haHA

--- atrem/src/Test/BetterLogic.java
+++ atrem/src/Test/BetterLogic.java
@@ -1,10 +1,34 @@
 package Test;
 
+import atrem.Connect4.Game.LastMove;
+import atrem.Connect4.Game.PlayerId;
 import atrem.Connect4.Game.board.Board;
 
 public class BetterLogic {
-	public void findWin(Board board) {
-		
+	LastMove lastMove;
+	Board board;
+	public void findWin(Board board, LastMove lastMove) {
+		this.lastMove = lastMove;
+		this.board = board;
 	}
-	public void 
+	public int checkDiagonalUP()
+	{
+		sameTokenCounter = 0;
+		for (int row = lastRow, slot = lastSlot; row < endRow
+				&& slot < endSlot; row++, slot++) {
+			if(board.getHoleState(row, slot) == board.playerIdtoHoleState(checkedPlayer))
+				sameTokenCounter++;
+			else 
+				break;
+		}
+}
+	}
+	public int sameTokenCounter;
+	public void checkStartWithEnd(int lastSlot, int lastRow, int endRow,
+			int endSlot) {
+		int sameTokenCounter = 0;
+		PlayerId checkedPlayer = lastMove.getPlayerId();
+		if ((endRow > lastRow) && (endSlot > lastSlot)) // gora skos
+			
+	}
 }
