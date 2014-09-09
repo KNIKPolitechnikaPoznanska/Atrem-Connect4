510bcb07e4bf5252ca6cbf1372cf6c043c70d45c
Lucas <llukas1994@wp.pl>
2014-09-09 13:30:27 +0200

    Executor on offfline mod

--- atrem/src/atrem/connect4/Connect4Client.java
+++ atrem/src/atrem/connect4/Connect4Client.java
@@ -6,13 +6,30 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import multiplayer.IConnect4Server;
+import atrem.connect4.console.Menu;
+import atrem.connect4.game.GameConfig;
+import atrem.connect4.game.GameFactory;
 
 public class Connect4Client {
-	private static IConnect4Server connect4Server;
+	private static GameFactory gameFactory;
+	private static GameConfig config;
+	private static Menu menu;
+	private IConnect4Server connect4Server;
 
-	public static void main(String[] args) throws RemoteException,
-			NotBoundException {
+	public Connect4Client() throws RemoteException, NotBoundException {
+
 		Registry r = LocateRegistry.getRegistry(1234);
 		connect4Server = (IConnect4Server) r.lookup("connect4Server");
+
+		gameFactory = new GameFactory();
+		config = new GameConfig(gameFactory);
+	}
+
+	public void runOnLocal() {
+		config.setDBox();
+	}
+
+	public void runOnServer() {
+		// config.showServerDBox();
 	}
 }
--- atrem/src/atrem/connect4/Connect4Server.java
+++ atrem/src/atrem/connect4/Connect4Server.java
@@ -11,13 +11,34 @@ import multiplayer.IConnect4Server;
 /**
  * Urruchamia serwer gry.
  */
-public class Connect4Server {
+public class Connect4Server implements Runnable {
 	private static IConnect4Server connect4Server;
 
+	/**
+	 * Uruchamia tylko serwer.
+	 * 
+	 * @param args
+	 * @throws RemoteException
+	 * @throws AlreadyBoundException
+	 */
 	public static void main(String[] args) throws RemoteException,
 			AlreadyBoundException {
 		connect4Server = new GameServer();
 		Registry registry = LocateRegistry.createRegistry(1234);
 		registry.bind("connect4Server", connect4Server);
+	}
+
+	/**
+	 * Metoda uruchamiana jako w¹tek w Offline.
+	 */
+	@Override
+	public void run() {
+		try {
+			connect4Server = new GameServer();
+			Registry registry = LocateRegistry.createRegistry(1234);
+			registry.bind("connect4LocalServer", connect4Server);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
