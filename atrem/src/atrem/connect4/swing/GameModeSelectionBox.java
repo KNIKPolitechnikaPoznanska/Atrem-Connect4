807dfa6899cbcea1ddc91f2333ba4e3277fb8a3d
Grzegorz <Admin@10.7.7.204>
2014-09-09 13:31:30 +0200

    qwd

--- atrem/src/atrem/connect4/swing/GameModeSelectionBox.java
+++ atrem/src/atrem/connect4/swing/GameModeSelectionBox.java
@@ -2,73 +2,88 @@ package atrem.connect4.swing;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JRadioButton;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.EmptyBorder;
 
 public class GameModeSelectionBox extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
+	private final JRadioButton online;
+	private final JRadioButton offline;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		try {
-			GameModeSelectionBox dialog = new GameModeSelectionBox();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
 	}
 
 	/**
 	 * Create the dialog.
 	 */
 	public GameModeSelectionBox() {
-		setBounds(100, 100, 140, 173);
+
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
+
+		contentPanel
+				.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		JLabel label = new JLabel("Wybierz tryb gry:");
+
+		ButtonGroup buttonGroup = new ButtonGroup();
+		online = new JRadioButton("online", true);
+		offline = new JRadioButton("offline", true);
+		buttonGroup.add(online);
+		buttonGroup.add(offline);
+
+		contentPanel.add(label);
+		contentPanel.add(online);
+		contentPanel.add(offline);
+
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
 		{
-			JLabel lblNewLabel = new JLabel("Wybierz tryb gry:");
-			contentPanel.add(lblNewLabel);
+			JButton okButton = new JButton("OK");
+			okButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+
+					if (online.isSelected()) {
+
+					}
+					if (offline.isSelected()) {
+
+					}
+
+				}
+			});
+			okButton.setActionCommand("OK");
+			buttonPane.add(okButton);
+			getRootPane().setDefaultButton(okButton);
 		}
 		{
-			JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Online");
-			rdbtnNewRadioButton_1.setSelected(true);
-			rdbtnNewRadioButton_1.setVerticalAlignment(SwingConstants.BOTTOM);
-			contentPanel.add(rdbtnNewRadioButton_1);
+			JButton cancelButton = new JButton("Cancel");
+			cancelButton.setActionCommand("Cancel");
+			buttonPane.add(cancelButton);
 		}
-		{
-			JRadioButton rdbtnNewRadioButton = new JRadioButton("Offline");
-			rdbtnNewRadioButton.setVerticalAlignment(SwingConstants.TOP);
-			contentPanel.add(rdbtnNewRadioButton);
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+
+		pack();
 	}
 
 }
