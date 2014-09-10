1a4927e18ab9a9c137ef234cfd83216e5f607920
Grzegorz <Admin@10.7.7.204>
2014-09-10 12:48:06 +0200

    adaafgfafwawaggwf

--- atrem/src/RMITest/RemoteGameControllerServer.java
+++ atrem/src/RMITest/RemoteGameControllerServer.java
@@ -1,9 +1,10 @@
 package RMITest;
 
 import java.awt.Point;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
-import atrem.connect4.game.GameController;
 import atrem.connect4.game.GameState;
 import atrem.connect4.game.LastMove;
 import atrem.connect4.game.Logic;
@@ -13,160 +14,167 @@ import atrem.connect4.game.board.HoleState;
 import atrem.connect4.game.player.PlayerController;
 import atrem.connect4.game.player.PlayerId;
 
-public class RemoteGameControllerServer implements GameController {
+public class RemoteGameControllerServer extends UnicastRemoteObject
+		implements
+			RemoteGameController {
+
+	protected RemoteGameControllerServer() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
-	public int move(int slot) {
+	public int move(int slot) throws RemoteException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public void startNewGame() {
+	public void startNewGame() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void wakeUpGCr() {
+	public void wakeUpGCr() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void analyseDecision() {
+	public void analyseDecision() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void run() {
+	public void run() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void startGameLoop() {
+	public void startGameLoop() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void backToMenu() {
+	public void backToMenu() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void wakeUp() {
+	public void wakeUp() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public HoleState getHoleState(int rows, int slots) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setResult(ResultState result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public PlayerId getPlayerTurn() {
+	public HoleState getHoleState(int rows, int slots) throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setPlayerTurn(PlayerId playerTurn) {
+	public void setResult(ResultState result) throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public int getEmptySpot() {
+	public PlayerId getPlayerTurn() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPlayerTurn(PlayerId playerTurn) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getEmptySpot() throws RemoteException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public ResultState getResult() {
+	public ResultState getResult() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Board getBoard() {
+	public Board getBoard() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setBoard(Board board) {
+	public void setBoard(Board board) throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public PlayerController getPlayer1() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setPlayer1(PlayerController player1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public PlayerController getPlayer2() {
+	public PlayerController getPlayer1() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setPlayer2(PlayerController player2) {
+	public void setPlayer1(PlayerController player1) throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public GameState getGamestate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public PlayerController getCurrentPlayer() {
+	public PlayerController getPlayer2() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public LastMove getLastMove() {
+	public void setPlayer2(PlayerController player2) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public GameState getGamestate() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setGamestate(GameState gamestate) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Logic getLogic() {
+	public PlayerController getCurrentPlayer() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Point> getWinningCoordinates() {
+	public LastMove getLastMove() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setGamestate(GameState gamestate) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Logic getLogic() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Point> getWinningCoordinates() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
