c6514629a7788330576bd4a415d22b5d1020993f
Mieloch <Mieloch@10.7.7.199>
2014-09-10 11:46:51 +0200

    dodane nowe klasy

---
+++ atrem/src/rmi/PlayerControllerService.java
@@ -0,0 +1,96 @@
+package rmi;
+
+import java.rmi.RemoteException;
+
+import atrem.connect4.game.GameController;
+import atrem.connect4.game.ResultState;
+import atrem.connect4.game.player.PlayerAttributes;
+import atrem.connect4.game.player.PlayerController;
+import atrem.connect4.game.player.PlayerId;
+
+public class PlayerControllerService implements PlayerController {
+
+	RemotePlayerController remotePlayerController;
+
+	@Override
+	public String getName() {
+
+		try {
+			return remotePlayerController.getName();
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+		return null;
+	}
+
+	@Override
+	public void setName(String playerName) {
+		try {
+			remotePlayerController.setName(playerName);
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+
+	}
+
+	@Override
+	public PlayerId getPlayerId() {
+		try {
+			return remotePlayerController.getPlayerId();
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+		return null;
+	}
+
+	@Override
+	public void yourTurn() {
+		try {
+			remotePlayerController.yourTurn();
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+
+	}
+
+	@Override
+	public void setGamecontroller(GameController gamecontroller) {
+		try {
+			remotePlayerController.setGamecontroller(gamecontroller);
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+
+	}
+
+	@Override
+	public void endOfGame(ResultState resultGame) {
+		try {
+			remotePlayerController.endOfGame(resultGame);
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+
+	}
+
+	@Override
+	public int getPlayerPoints() {
+		try {
+			return remotePlayerController.getPlayerPoints();
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+		return 0;
+	}
+
+	@Override
+	public PlayerAttributes getPlayerAttributes() {
+		try {
+			return remotePlayerController.getPlayerAttributes();
+		} catch (RemoteException e) {
+			System.out.println("Wyjatek w remotePlayerController");
+		}
+		return null;
+	}
+
+}
--- atrem/src/rmi/RemotePlayerController.java
+++ atrem/src/rmi/RemotePlayerController.java
@@ -9,14 +9,13 @@ import atrem.connect4.game.player.PlayerAttributes;
 import atrem.connect4.game.player.PlayerId;
 
 public interface RemotePlayerController extends Remote {
+	public String getName() throws RemoteException;
 
 	public abstract void setName(String playerName) throws RemoteException;
 
 	public abstract PlayerId getPlayerId() throws RemoteException;
 
 	public abstract void yourTurn() throws RemoteException;
-
-	// public abstract void refreshView(int row, int slot);
 
 	public abstract void setGamecontroller(GameController gamecontroller)
 			throws RemoteException;
