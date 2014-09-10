413b0f80cd5da88d2b9fb87f6d9e9db68eafe80f
Patryk <Patryk@10.7.7.152>
2014-09-10 12:23:36 +0200

    cp do gc

--- atrem/src/testy/game/remote/Player1.java
+++ atrem/src/testy/game/remote/Player1.java
@@ -5,19 +5,25 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import atrem.connect4.game.GameController;
-import atrem.connect4.game.player.PlayerController;
-import atrem.connect4.swing.SwingPresenter;
-
 public class Player1 {
 
 	public static void main(String[] args) throws RemoteException,
 			NotBoundException, InterruptedException {
 		// TODO Auto-generated method stub
-		Registry r = LocateRegistry.getRegistry(1234);
-		GameController Connect4 = (GameController) r.lookup("Connect4");
 
-		PlayerController player1 = new SwingPresenter(null, null, null, 0);
+		// GameController Connect4 = (GameController) r.lookup("connect");
+		// LocalPlayerController player = new LocalPlayerControlerImpl();
+		/**
+		 * connect
+		 */
+		Registry r = LocateRegistry.getRegistry("locahost", 1254);
+		RemotePlayerController connect = (RemotePlayerController) r
+				.lookup("connect");
+		/**
+		 * 
+		 */
+
+		LocalGameController gameController = new LocalGameController();
 
 	}
 
